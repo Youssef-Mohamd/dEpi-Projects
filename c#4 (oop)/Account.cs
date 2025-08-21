e2c0946217feec03416f0d44ff54fb3c86a2c134
Youssef Mohamed <youssefmohamed1001.at@gmail.com>
2025-08-21 15:48:31 +0300

    Account.cs

--- c#4 (oop)/Account.cs
+++ c#4 (oop)/Account.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Transactions;
+
 
 namespace ConsoleApp4
 {
@@ -111,3 +107,4 @@ this.Withdraw(amount);  // withdraw from account
     }
 }
    
+
