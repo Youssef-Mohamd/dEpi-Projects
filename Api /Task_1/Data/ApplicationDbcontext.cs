69c0b6af7be5ccdf0837e5da2f2b904fec7b1763
Youssef Mohamed <youssefmohamed1001.at@gmail.com>
2025-09-21 17:34:19 +0300

    Add files via upload

---
+++ Api /Task_1/Data/ApplicationDbcontext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbcontext : DbContext
+    {
+        public ApplicationDbcontext(DbContextOptions<ApplicationDbcontext> options) : base(options)
+        {
+
+        }
+
+
+
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
