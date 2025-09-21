af02f91c8f90315ca14ae232a8c313deba795030
Youssef Mohamed <youssefmohamed1001.at@gmail.com>
2025-09-21 16:44:19 +0300

    Add files via upload

---
+++ Api/Task_2/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace Api_Task__2.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+
+    }
+
+
+}
